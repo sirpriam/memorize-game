10dbe21e6fa6c7248a323ebc1eb7764951507b61
priambodo.pangestu <priambodo.pangestu@primecash.co.id>
2025-09-22 08:48:02 +0700

    Initial Commit

---
+++ vignette_memorize_22_sept/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vignette_memorize_22_sept
+//
+//  Created by Pangestu, Priambodo on 22/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vignette_memorize_22_sept/vignette_memorize_22_septApp.swift
@@ -0,0 +1,17 @@
+//
+//  vignette_memorize_22_septApp.swift
+//  vignette_memorize_22_sept
+//
+//  Created by Pangestu, Priambodo on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct vignette_memorize_22_septApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
